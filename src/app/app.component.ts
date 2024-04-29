7fdc3a9aeee3f27095e7541e7c9b5900fae74894
Anton <vladykin.anton@gmail.com>
2024-04-29 20:55:14 +0300

    feat: add file input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-web-workers';
+  public title = 'Angular web worker example';
+
+  public loadImage(e: Event): void {
+    const image = (e.target as HTMLInputElement).files![0];
+    console.log(`Image ${image.name} loaded`);
+  }
 }
